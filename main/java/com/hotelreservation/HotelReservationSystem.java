87f4c3b155aa379e3a5a9a1a2e98d48c1cf4b50c
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-15 09:09:45 +0530

    added:welcome in hotel management system.

---
+++ main/java/com/hotelreservation/HotelReservationSystem.java
@@ -0,0 +1,7 @@
+package com.hotelreservation;
+
+public class HotelReservationSystem {
+    public static void main (String[] args){
+        System.out.println("Welcome in Hotel Reservation System");
+    }
+}
