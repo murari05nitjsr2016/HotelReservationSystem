2aeedf4946e3f4eabeed35f8a8ab07af443787ea
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-15 09:19:04 +0530

    added:hotel name and its rate for regular customer

--- main/java/com/hotelreservation/HotelReservationSystem.java
+++ main/java/com/hotelreservation/HotelReservationSystem.java
@@ -1,7 +1,17 @@
 package com.hotelreservation;
 
+import java.util.Scanner;
+
 public class HotelReservationSystem {
+
     public static void main (String[] args){
         System.out.println("Welcome in Hotel Reservation System");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Hotel Name");
+        String hotelName = sc.next();
+        System.out.println("Enter the  Hotel Rates for Regular Customer");
+        float nRate = sc.nextFloat();
+
+
     }
 }
