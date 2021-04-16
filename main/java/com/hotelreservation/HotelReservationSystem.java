8be1d3412c80c5c4d741d7537c2fbe4fc7ccae05
murari05nitjsr2016 <murari05nitjsr2016@gmail.com>
2021-04-16 08:57:33 +0530

    added : cheapest hotel

--- main/java/com/hotelreservation/HotelReservationSystem.java
+++ main/java/com/hotelreservation/HotelReservationSystem.java
@@ -12,6 +12,19 @@ public  static  boolean addHotel(Hotel hotel){
    return hotelList.add(hotel);
 }
 
+    public static  Hotel cheapestHotel(ArrayList<Hotel> hotelList,String startingDate,String lastDate) throws ParseException {
+        SimpleDateFormat formatter=new SimpleDateFormat("ddMMMyyyy");
+        Date date1 = formatter.parse(startingDate);
+        Date date2 = formatter.parse(lastDate);
+        long diff = date2.getTime() - date1.getTime();
+        int days = (int) (diff / (1000*60*60*24));
+        System.out.println("Days between: " + days);
+        Hotel hotel = hotelList.stream().min(Comparator.comparingInt(Hotel::getRegularCustomerRate)).get();
+        int  TotalRates = days * hotel.getRegularCustomerRate();
+        System.out.println("Hotel Name - "+hotel.hotelName +", TotalRate:"+TotalRates);
+        return hotel;
+    }
+
     public static void main (String[] args) throws ParseException {
         System.out.println("Welcome in Hotel Reservation System");
         HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
@@ -21,5 +34,9 @@ public  static  boolean addHotel(Hotel hotel){
         addHotel(hotel2);
         Hotel hotel3 = new Hotel("RidgeWood",220);
         hotelReservationSystem.addHotel(hotel3);
+        String startingDate = "10sep2020";
+        String lastDate = "11sep2020";
+        cheapestHotel(hotelList,startingDate,lastDate);
+
     }
 }
--- test/java/com/hotelreservationtest/hotelReservationSystemTest.java
+++ test/java/com/hotelreservationtest/hotelReservationSystemTest.java
@@ -5,6 +5,11 @@ import com.hotelreservation.HotelReservationSystem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static com.hotelreservation.HotelReservationSystem.cheapestHotel;
+
 public class hotelReservationSystemTest {
 
     @Test
@@ -14,6 +19,17 @@ public class hotelReservationSystemTest {
         Hotel hotel = new Hotel("LakeWood",110);
         boolean result =  hotelReservationSystem.addHotel(hotel);
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void InGivenDateSlotShouldReturnChepeastHotelName() throws ParseException {
+        HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
+        String startingDate = "10sep2020";
+        String lastDate = "11sep2020";
+        ArrayList<Hotel> hotelList = new ArrayList<>();
+        Hotel hotel =  cheapestHotel(hotelList,startingDate,lastDate);
+        Assertions.assertEquals("LakeWood",hotel);
+        
     }
 
 }
